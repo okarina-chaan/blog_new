652eae0b48bc68f2b3c9872614bb4fe13277e044
おかりな_69B <lcttt.pc@gmail.com>
2025-10-13 02:28:30 +0900

    トップページを作った

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,11 @@
+require 'ostruct'
+
 class PagesController < ApplicationController
   def home
+    @articles = [
+      OpenStruct.new(title: "サンプル記事1", summary: "これはテスト用の説明文です", url: "#"),
+      OpenStruct.new(title: "サンプル記事2", summary: "カードのデザイン確認用です", url: "#"),
+      OpenStruct.new(title: "サンプル記事3", summary: "後でAPIとつなぎます", url: "#")
+    ]
   end
 end
