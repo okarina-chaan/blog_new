9026476b3416ef6bba2341de578504b4f956309c
おかりな_69B <lcttt.pc@gmail.com>
2025-10-14 10:17:22 +0900

    トップページのルーティングの設定

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,6 @@
 require "ostruct"
 
 class PagesController < ApplicationController
-  def home
-    @articles = [
-      OpenStruct.new(title: "サンプル記事1", summary: "これはテスト用の説明文です", url: "#"),
-      OpenStruct.new(title: "サンプル記事2", summary: "カードのデザイン確認用です", url: "#"),
-      OpenStruct.new(title: "サンプル記事3", summary: "後でAPIとつなぎます", url: "#")
-    ]
+  def home ;
   end
 end
