dab12170b3f132f12df2410cd0052722a962ece6
おかりな_69B <lcttt.pc@gmail.com>
2026-02-12 13:32:27 +0900

    コントローラにいれてたポートフォリをサービスクラスに移動

---
+++ app/services/portfolio_service.rb
@@ -0,0 +1,13 @@
+class PortfolioService
+  def self.projects
+    [
+      {
+        title: "月めぐるノート",
+        description: "月の満ち欠けと日記を記録できるアプリ。\n日々の振り返りのサイクルを月相と組み合わせてみました。",
+        image: "portfolio/tsukimeguru-note.png",
+        github_url: "https://github.com/okarina-chaan/tsukimeguru_note",
+        site_url: "https://tsukimeguru-note.com"
+      }
+    ]
+  end
+end
